db759bf58e8ab44d16de268b813b651e06566470
Mikkonen Jussi TTK17SP <jussimikkonen1@kamk.fi>
2017-12-01 19:38:34 +0200

    Added movement

--- New Unity Project/Assets/Scripts/PlayerController.cs
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -6,16 +6,23 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
     Transform playerTransform;
-    Rigidbody2D myBody;
+    Rigidbody2D rb;
     // Use this for initialization
     void Start()
     {
-
+       rb = GetComponent<Rigidbody2D>();
+        playerTransform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-
+        Move(Input.GetAxisRaw("Horizontal"));
+    }
+    public void Move(float horizontalInput)
+    {
+        Vector2 moveVel = rb.velocity;
+        moveVel.x = horizontalInput * speed;
+        rb.velocity = moveVel;
     }
 }
