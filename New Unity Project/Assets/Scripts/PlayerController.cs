72712248bb4e26e70f9033dfc464a1c975a83ebd
Mikkonen Jussi TTK17SP <jussimikkonen1@kamk.fi>
2017-12-01 20:27:32 +0200

    Timer is done

--- New Unity Project/Assets/Scripts/PlayerController.cs
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -5,13 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
-    Transform playerTransform;
     Rigidbody2D rb;
     // Use this for initialization
     void Start()
     {
-       rb = GetComponent<Rigidbody2D>();
-        playerTransform = GetComponent<Transform>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
