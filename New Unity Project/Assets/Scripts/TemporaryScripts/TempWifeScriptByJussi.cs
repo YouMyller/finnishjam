8ef394509926d2b9929b3374b66162621d3f9622
Myllylä Juuso TTK17SP <juusomyllyla1@kamk.fi>
2017-12-02 20:29:22 +0200

    Ole hyvä Nikke

--- New Unity Project/Assets/Scripts/TemporaryScripts/TempWifeScriptByJussi.cs
+++ New Unity Project/Assets/Scripts/TemporaryScripts/TempWifeScriptByJussi.cs
@@ -6,16 +6,27 @@ public class TempWifeScriptByJussi : MonoBehaviour
 {
     public float speed;
     public float defaultSpeed;
+    public Animator anim;
     Rigidbody2D rb;
     // Use this for initialization
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         rb.AddForce(Vector2.right * speed);
+        anim.SetFloat("Speed", speed);
+        if (transform.position.y > 1)
+        {
+            anim.SetBool("isGrounded", false);
+        }
+        else
+        {
+            anim.SetBool("isGrounded", true);
+        }
     }
 }
